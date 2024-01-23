b63d5d3bc6db88e5c722e14c54cc10510310d09d
aleksa <zabumaphu@gmail.com>
2024-01-22 20:39:28 -0500

    String formating excersises begun.

---
+++ ch2/pun2.c
@@ -0,0 +1,12 @@
+/* Name: pun2.c
+   Purpose: prints a bad pun
+   Author: Aleksa Zatezalo */
+   
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C or not to C:");
+    printf("that is the question\n");
+    return 0;
+}
