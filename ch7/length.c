0172f05b19e2b93eff6a03a4d083b11055dfb96f
aleksa <zabumaphu@gmail.com>
2024-02-01 23:53:54 -0500

    Chars recived.

--- ch7/length.c
+++ ch7/length.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 int main(void){
-    int len = 0;
+    int lens = 0;
 
     printf("Enter a message: ");
     while(getchar() != '\n'){
-        len++;
+        lens++;
     }
 
-    printf("Your message is %d chars long\n", len);
+    printf("Your message is %d chars long\n", lens);
 }
